80df361f3d7b53c54d200af66f83edd70b82120f
Marceloezio <128635263+Marceloezio@users.noreply.github.com>
2024-03-27 15:50:40 -0300

    js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-let nomeUsuário = prompt("Qual o seu nome?");
+let nomeUsuário = ""; 
 let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual o seu nome?");
+}
 elemento.textContent = nomeUsuário;
